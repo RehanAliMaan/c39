98b8c0badfc94aa3674057dff77fe35c95a01d07
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2023-11-26 19:43:26 +0500

    Add files via upload

---
+++ Untitled7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+		cout<<"please enter three numbers:"<<"15"<<" "<<"35"<<" "<<"72"<<endl;
+
+	cout<<"your numbers forward"<<endl;
+	cout<<"15"<<endl;
+	cout<<"35"<<endl;
+	cout<<"72"<<endl;
+	cout<<"your numbers reversed"<<endl;
+	cout<<"72"<<endl;
+	cout<<"35"<<endl;
+	cout<<"15"<<endl;
+
+
+
+}
